37623788f3086c5995fa4b2042a80c27d23c80b1
Alizon Kingslen <kingslenalizon@icloud.com>
2026-02-13 22:41:33 -0400

    implemented home page

--- app/(homePage)/page.tsx
+++ app/(homePage)/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
     return (
         <div className={styles.page}>
-            <h1>hello world</h1>
+            <h1>Conheça as Naves do Universo Star Wars</h1>
+            <Link href={"/spaceships"} className={styles.seeAllBtn}>
+                Ver todos
+            </Link>
         </div>
     );
 }
